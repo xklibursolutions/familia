f63a3cc3771537f4cf4aeda2c76a668b10f2317f
Jonathan St-Michel <j.stmichel@xklibursolutions.io>
2024-05-06 15:34:58 -0400

    feat: Add api versioning base code and refactor program file.

--- src/IdentityApi/Extensions/ApplicationMappingExtension.cs
+++ src/IdentityApi/Extensions/ApplicationMappingExtension.cs
@@ -1,4 +1,6 @@
 ﻿using System.Security.Claims;
+using Asp.Versioning;
+using Asp.Versioning.Builder;
 using Microsoft.AspNetCore.Mvc;
 using XkliburSolutions.Familia.Domain.Constants;
 using XkliburSolutions.Familia.Domain.Models;
@@ -18,35 +20,48 @@ public static class ApplicationMappingExtension
     /// <returns>The configured <see cref="WebApplication"/>.</returns>
     public static WebApplication ConfigureMapping(this WebApplication app)
     {
-        // Map the "/api/auth" endpoint for user login
-        app.MapPost("/api/auth", async ([FromBody] Authentication authenticationModel, AuthenticationHandler authenticationHandler) =>
+        ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+            .HasApiVersion(new ApiVersion(1, 0))
+            .ReportApiVersions()
+            .Build();
+
+        RouteGroupBuilder apiVersionGroup = app
+            .MapGroup("api/v{version:apiVersion}")
+            .WithApiVersionSet(apiVersionSet)
+            .WithOpenApi();
+
+        // Map the "/auth" endpoint for user login
+        apiVersionGroup.MapPost("/auth", async ([FromBody] Authentication authenticationModel, AuthenticationHandler authenticationHandler) =>
         {
             return await authenticationHandler.AuthenticateAsync(authenticationModel);
         })
+        .MapToApiVersion(1, 0)
         .Produces(StatusCodes.Status400BadRequest)
         .Produces(StatusCodes.Status401Unauthorized)
         .Produces<AuthenticationResponse>(StatusCodes.Status200OK)
-        .WithName("Authenticate")
-        .WithOpenApi();
+        .WithName("Authenticate");
 
-        // Map the "/api/register" endpoint for user registration
-        app.MapPost("/api/register", async ([FromBody] Registration registrationModel, RegistrationHandler registrationHandler) =>
+        // Map the "/register" endpoint for user registration
+        apiVersionGroup.MapPost("/register", async ([FromBody] Registration registrationModel, RegistrationHandler registrationHandler) =>
         {
             return await registrationHandler.RegisterAsync(registrationModel, [ApplicationRoles.User]);
         })
+        .MapToApiVersion(1, 0)
         .Produces(StatusCodes.Status500InternalServerError)
         .Produces<Response>(StatusCodes.Status200OK)
-        .WithName("Register")
-        .WithOpenApi();
+        .WithName("Register");
 
-        // Map the "/api/user" endpoint to retrieve user information
-        app.MapGet("/api/users/me", async (ClaimsPrincipal user, UsersHandler usersHandler) =>
+        // Map the "/user" endpoint to retrieve user information
+        apiVersionGroup.MapGet("/users/me", async (ClaimsPrincipal user, UsersHandler usersHandler) =>
         {
             return await usersHandler.GetUserAsync(user);
         })
+        .MapToApiVersion(1, 0)
         .RequireAuthorization()
-        .WithName("Me")
-        .WithOpenApi();
+        .Produces(StatusCodes.Status400BadRequest)
+        .Produces(StatusCodes.Status401Unauthorized)
+        .Produces<DataResponse<User>>(StatusCodes.Status200OK)
+        .WithName("Me");
 
         return app;
     }
--- src/IdentityApi/Program.cs
+++ src/IdentityApi/Program.cs
@@ -1,10 +1,5 @@
-using System.Reflection;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using XkliburSolutions.Familia.Domain.Entities;
 using XkliburSolutions.Familia.Extensions;
 using XkliburSolutions.Familia.Infrastructure.Persistence;
@@ -19,68 +14,24 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<IdentityApiDbContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.SaveToken = true;
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidAudience = configuration["JWT:ValidAudience"],
-        ValidIssuer = configuration["JWT:ValidIssuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]!))
-    };
-});
-
+builder.Services.AddAuthenticationConfiguration(configuration);
 builder.Services.AddAuthorization();
-
 builder.Services.AddLocalization();
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Identity API", Version = "v1" });
-    string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-
-    // Configure JWT Bearer authentication for Swagger
-    OpenApiSecurityScheme securityScheme = new()
-    {
-        Name = "Authorization",
-        Description = "Enter 'Bearer {token}'",
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
-    };
-
-    c.AddSecurityDefinition("Bearer", securityScheme);
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            securityScheme,
-            new List<string>()
-        }
-    });
-});
-
 builder.Services.AddApplicationServices();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioningConfiguration();
+builder.Services.AddSwaggerConfiguration();
 
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint($"/swagger/v1/swagger.json", "Identity Api v1.0");
+    });
 
     using (IServiceScope scope = app.Services.CreateScope())
     {
